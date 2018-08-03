68bb8a947bc9705f53d474cddc5915c96960ef9c
Rahul Goyal <rahulgoyal0220@gmail.com>
2018-08-02 23:25:32 -0400

    Add-on Product

--- product-details.php
+++ product-details.php
@@ -439,7 +439,11 @@ if(isset($_POST['submit']))
                         <div class="col-sm-3">
                             <ul id="product-tabs" class="nav nav-tabs nav-tab-cell">
                                 <li class="active"><a data-toggle="tab" href="#description">DESCRIPTION</a></li>
-                                <li><a data-toggle="tab" href="#review">REVIEW</a></li>
+                                <?php if($row['category']== 3)
+                                {
+                                ?>
+                                <li><a data-toggle="tab" href="#review">Add On</a></li>
+                                <?php } ?>
                             </ul><!-- /.nav-tabs #product-tabs -->
                         </div>
                         <div class="col-sm-9">
@@ -451,112 +455,34 @@ if(isset($_POST['submit']))
                                         <p class="text"><?php echo $row['productDescription'];?></p>
                                     </div>
                                 </div><!-- /.tab-pane -->
-
+                                <?php if($row['category']== 3)
+                                {
+                                ?>
                                 <div id="review" class="tab-pane">
                                     <div class="product-tab">
 
                                         <div class="product-reviews">
-                                            <h4 class="title">Customer Reviews</h4>
-                                            <?php $qry=mysqli_query($con,"select * from productreviews where productId='$pid'");
+                                            <h4 class="title">Choose add ons</h4>
+                                            <?php $qry=mysqli_query($con,"select * from products where category=0");
                                             while($rvw=mysqli_fetch_array($qry))
                                             {
                                             ?>
 
-                                            <div class="reviews" style="border: solid 1px #000; padding-left: 2% ">
-                                                <div class="review">
-                                                    <div class="review-title"><span class="summary"><?php echo htmlentities($rvw['summary']);?></span><span class="date"><i class="fa fa-calendar"></i><span><?php echo htmlentities($rvw['reviewDate']);?></span></span></div>
-
-                                                    <div class="text">"<?php echo htmlentities($rvw['review']);?>"</div>
-                                                    <div class="text"><b>Quality :</b>  <?php echo htmlentities($rvw['quality']);?> Star</div>
-                                                    <div class="text"><b>Price :</b>  <?php echo htmlentities($rvw['price']);?> Star</div>
-                                                    <div class="text"><b>value :</b>  <?php echo htmlentities($rvw['value']);?> Star</div>
-                                                    <div class="author m-t-15"><i class="fa fa-pencil-square-o"></i> <span class="name"><?php echo htmlentities($rvw['name']);?></span></div>													</div>
+                                            <div>
+                                                <div>
+                                                    <div>
+                                                        <span class="summary"><?php echo htmlentities($rvw['productName']);?>
+                                                        </span>
+                                                      </div>
 
                                             </div>
                                             <?php } ?><!-- /.reviews -->
                                         </div><!-- /.product-reviews -->
-                                        <form role="form" class="cnt-form" name="review" method="post">
-
-
-                                            <div class="product-add-review">
-                                                <h4 class="title">Write your own review</h4>
-                                                <div class="review-table">
-                                                    <div class="table-responsive">
-                                                        <table class="table table-bordered">
-                                                            <thead>
-                                                            <tr>
-                                                                <th class="cell-label">&nbsp;</th>
-                                                                <th>1 star</th>
-                                                                <th>2 stars</th>
-                                                                <th>3 stars</th>
-                                                                <th>4 stars</th>
-                                                                <th>5 stars</th>
-                                                            </tr>
-                                                            </thead>
-                                                            <tbody>
-                                                            <tr>
-                                                                <td class="cell-label">Quality</td>
-                                                                <td><input type="radio" name="quality" class="radio" value="1"></td>
-                                                                <td><input type="radio" name="quality" class="radio" value="2"></td>
-                                                                <td><input type="radio" name="quality" class="radio" value="3"></td>
-                                                                <td><input type="radio" name="quality" class="radio" value="4"></td>
-                                                                <td><input type="radio" name="quality" class="radio" value="5"></td>
-                                                            </tr>
-                                                            <tr>
-                                                                <td class="cell-label">Price</td>
-                                                                <td><input type="radio" name="price" class="radio" value="1"></td>
-                                                                <td><input type="radio" name="price" class="radio" value="2"></td>
-                                                                <td><input type="radio" name="price" class="radio" value="3"></td>
-                                                                <td><input type="radio" name="price" class="radio" value="4"></td>
-                                                                <td><input type="radio" name="price" class="radio" value="5"></td>
-                                                            </tr>
-                                                            <tr>
-                                                                <td class="cell-label">Value</td>
-                                                                <td><input type="radio" name="value" class="radio" value="1"></td>
-                                                                <td><input type="radio" name="value" class="radio" value="2"></td>
-                                                                <td><input type="radio" name="value" class="radio" value="3"></td>
-                                                                <td><input type="radio" name="value" class="radio" value="4"></td>
-                                                                <td><input type="radio" name="value" class="radio" value="5"></td>
-                                                            </tr>
-                                                            </tbody>
-                                                        </table><!-- /.table .table-bordered -->
-                                                    </div><!-- /.table-responsive -->
-                                                </div><!-- /.review-table -->
-
-                                                <div class="review-form">
-                                                    <div class="form-container">
-
-
-                                                        <div class="row">
-                                                            <div class="col-sm-6">
-                                                                <div class="form-group">
-                                                                    <label for="exampleInputName">Your Name <span class="astk">*</span></label>
-                                                                    <input type="text" class="form-control txt" id="exampleInputName" placeholder="" name="name" required="required">
-                                                                </div><!-- /.form-group -->
-                                                                <div class="form-group">
-                                                                    <label for="exampleInputSummary">Summary <span class="astk">*</span></label>
-                                                                    <input type="text" class="form-control txt" id="exampleInputSummary" placeholder="" name="summary" required="required">
-                                                                </div><!-- /.form-group -->
-                                                            </div>
-
-                                                            <div class="col-md-6">
-                                                                <div class="form-group">
-                                                                    <label for="exampleInputReview">Review <span class="astk">*</span></label>
-
-                                                                    <textarea class="form-control txt txt-review" id="exampleInputReview" rows="4" placeholder="" name="review" required="required"></textarea>
-                                                                </div><!-- /.form-group -->
-                                                            </div>
-                                                        </div><!-- /.row -->
-
-                                                        <div class="action text-right">
-                                                            <button name="submit" class="btn btn-primary btn-upper">SUBMIT REVIEW</button>
-                                                        </div><!-- /.action -->
-
-                                        </form><!-- /.cnt-form -->
                                     </div><!-- /.form-container -->
                                 </div><!-- /.review-form -->
 
                             </div><!-- /.product-add-review -->
+                                <?php } ?>
 
                         </div><!-- /.product-tab -->
                     </div><!-- /.tab-pane -->
